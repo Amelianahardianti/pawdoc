b2ee9c03b6aeb771acb05f6e5df780cd7e2c4293
Amelianahardiantiutari@mail.ugm.ac.id <amelianahardiantiutari@mail.ugm.ac.id>
2024-12-19 21:55:48 +0700

    udah bener tinggal delete

--- SetUpProfileOwnerPage.xaml.cs
+++ SetUpProfileOwnerPage.xaml.cs
@@ -26,9 +26,39 @@ namespace pawdoc
             // Replace with your Firebase URL
         }
 
+        // Navigate to ClinicPage
+        private void ClinicButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ClinicPage());
+        }
+
+        // Navigate to VetListPage (this page itself)
+        private void VetListButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new VetListPage());
+        }
+
+        // Navigate to SetUpDiaryPage
+        private void InputDiaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new SetUpDiaryPage());
+        }
+
+        // Navigate to PetDiary page
+        private void PetDiary_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new PetDiary());
+        }
+
+        // Navigate to SetUpProfileOwnerPage
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new SetUpProfileOwnerPage());
+        }
+
+        // Navigate to Dashboard
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            // Navigasi kembali ke halaman sebelumnya
             this.NavigationService.Navigate(new DashboardPage(((selo)Application.Current.MainWindow).user));
 
         }
